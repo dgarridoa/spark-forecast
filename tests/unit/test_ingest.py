2e591b6b31a16c402476eda94d4b2aab3f54c405
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2023-02-24 18:12:26 -0300

    test(refactor): replace sales data by code

--- tests/unit/test_ingest.py
+++ tests/unit/test_ingest.py
@@ -1,6 +1,9 @@
 import logging
+import os
+from datetime import date, timedelta
 
 import mlflow
+import pandas as pd
 import pytest
 from pyspark.sql import SparkSession
 
@@ -11,18 +14,46 @@ from dbx_demand_forecast.utils import read_csv, read_delta_table
 conf = {
     "env": "default",
     "experiment": "/Shared/dbx_demand_forecast/dev_demand_forecast",
-    "input": {"path": "tests/unit/data/sales", "sep": ","},
+    "input": {"path": "", "sep": ","},
     "output": {
-        "path": "tests/unit/data/input",
+        "path": "",
         "database": "default",
         "table": "input",
     },
 }
 
 
+def update_conf(spark: SparkSession):
+    warehouse_dir = spark.conf.get("spark.hive.metastore.warehouse.dir")
+    conf["input"]["path"] = os.path.join(warehouse_dir, "sales")
+    conf["output"]["path"] = os.path.join(warehouse_dir, "input")
+
+
+def create_sales_csv(spark: SparkSession) -> None:
+    df = spark.createDataFrame(
+        pd.DataFrame(
+            {
+                "date": [date(2018, 12, 1) + timedelta(i) for i in range(30)],
+                "store": 1,
+                "item": 1,
+                "sales": map(float, range(1, 31)),
+            }
+        ),
+        schema=SalesSchema,
+    )
+    df.write.csv(
+        conf["input"]["path"],
+        header=True,
+        sep=conf["input"]["sep"],
+        mode="overwrite",
+    )
+
+
 @pytest.fixture(scope="session", autouse=True)
 def launch_ingestion_task(spark: SparkSession):
     logging.info(f"Launching {IngestionTask.__name__}")
+    update_conf(spark)
+    create_sales_csv(spark)
     ingestion_task = IngestionTask(spark, conf)
     ingestion_task.launch()
     logging.info(f"Launching the {IngestionTask.__name__} - done")
