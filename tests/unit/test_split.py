ee4eb6819d0e22d5f60c2f9f5f86199cb8ead168
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2023-08-26 20:49:17 -0400

    test: add test_split to test Split

--- tests/unit/test_split.py
+++ tests/unit/test_split.py
@@ -1,17 +1,17 @@
 from datetime import date, timedelta
 
 import pandas as pd
-from poc_forecast_cm.schema import SalesSchema, SplitSchema
-from poc_forecast_cm.split import Split
 from pyspark.sql import Row, SparkSession
 
-from tests.unit.utils import assert_pyspark_df_equal
+from dbx_demand_forecast.schema import SalesSchema, SplitSchema
+from dbx_demand_forecast.split import Split
+from tests.utils import assert_pyspark_df_equal
 
 execution_date = date(2022, 9, 12)
 time_delta = 2 * 52 * 7
-group_columns = ["location_id"]
-time_column = "tran_start_dt"
-target_column = "q_trx"
+group_columns = ["store", "item"]
+time_column = "date"
+target_column = "sales"
 test_size = 14
 
 
@@ -19,14 +19,16 @@ def test_split(spark: SparkSession):
     df_sales = spark.createDataFrame(
         [
             Row(
-                location_id="T104",
-                tran_start_dt=date(2022, 8, 29),
-                q_trx=1.0,
+                date=date(2022, 8, 29),
+                store=1,
+                item=1,
+                sales=1.0,
             ),
             Row(
-                location_id="T104",
-                tran_start_dt=date(2022, 9, 10),
-                q_trx=7.0,
+                date=date(2022, 9, 10),
+                store=1,
+                item=1,
+                sales=7.0,
             ),
         ],
         schema=SalesSchema,
@@ -35,11 +37,10 @@ def test_split(spark: SparkSession):
     df_expected_split = spark.createDataFrame(
         pd.DataFrame(
             {
-                "location_id": "T104",
-                "tran_start_dt": [
-                    date(2022, 8, 29) + timedelta(i) for i in range(14)
-                ],
-                "q_trx": [1.0] + [0.0] * 11 + [7.0, 0.0],
+                "store": 1,
+                "item": 1,
+                "date": [date(2022, 8, 29) + timedelta(i) for i in range(14)],
+                "sales": [1.0] + [0.0] * 11 + [7.0, 0.0],
                 "split:": (["train"] * 7 + ["test"] * 7),
             }
         ),
@@ -54,6 +55,6 @@ def test_split(spark: SparkSession):
         execution_date,
         time_delta,
     )
-    df_split = split.transform(df_sales)
+    df_split = split.transform(df_sales, SplitSchema)
 
     assert_pyspark_df_equal(df_expected_split, df_split)
