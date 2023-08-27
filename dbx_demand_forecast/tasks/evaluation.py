0f829205c67bdd1ab5356c0bdb1fd7e129b27a87
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2023-08-26 23:11:32 -0400

    refactor: use Evaluation on EvaluationTask

--- dbx_demand_forecast/tasks/evaluation.py
+++ dbx_demand_forecast/tasks/evaluation.py
@@ -1,78 +1,35 @@
-from collections.abc import Callable
+from datetime import date, datetime, timedelta
+from typing import Optional
 
-import darts.metrics
 import mlflow
-import pandas as pd
 import pyspark.sql.functions as F
-from darts.timeseries import TimeSeries
+from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.types import StructType
 
 from dbx_demand_forecast.common import Task
+from dbx_demand_forecast.evaluation import Evaluation
 from dbx_demand_forecast.schema import ForecastSchema, MetricsSchema
-from dbx_demand_forecast.utils import (
-    extract_timeseries_from_pandas_dataframe,
-    read_delta_table,
-    write_delta_table,
-)
-
-
-class Metrics:
-    def __init__(
-        self,
-        group_columns: list[str],
-        time_column: str,
-        target_column: str,
-    ):
-        self.group_columns = ["model"] + group_columns
-        self.time_column = time_column
-        self.target_column = target_column
-
-    def set_group_columns(self, df: pd.DataFrame) -> None:
-        self.group_columns_values = df.loc[0, self.group_columns].to_dict()
-
-    def evaluate(
-        self,
-        df: pd.DataFrame,
-        metric: Callable[[TimeSeries, TimeSeries], float],
-    ) -> pd.DataFrame:
-        self.set_group_columns(df)
-        y = extract_timeseries_from_pandas_dataframe(df, self.time_column, "y")
-        y_pred = extract_timeseries_from_pandas_dataframe(
-            df, self.time_column, "y_pred"
-        )
-        metric_value = metric(y, y_pred)
-        df_metric = pd.DataFrame(
-            [
-                {
-                    **self.group_columns_values,
-                    "metric": metric.__name__,
-                    "value": metric_value,
-                }
-            ]
-        )
-        return df_metric
-
-    @staticmethod
-    def get_metrics(
-        metrics: list[Callable[[TimeSeries, TimeSeries], float]],
-        df: pd.DataFrame,
-        group_columns: list[str],
-        time_column: str,
-        target_column: str,
-    ) -> pd.DataFrame:
-        df_metrics = pd.concat(
-            [
-                Metrics(group_columns, time_column, target_column).evaluate(
-                    df, metric
-                )
-                for metric in metrics
-            ]
-        )
-        return df_metrics
+from dbx_demand_forecast.utils import read_delta_table, write_delta_table
 
 
 class EvaluationTask(Task):
+    def __init__(
+        self,
+        spark: Optional[SparkSession] = None,
+        init_conf: Optional[dict] = None,
+    ) -> None:
+        super().__init__(spark, init_conf)
+        if "execution_date" not in self.conf:
+            execution_date = date.today() - timedelta(
+                days=date.today().weekday()
+            )
+        else:
+            execution_date = datetime.strptime(
+                self.conf["execution_date"], "%Y-%m-%d"
+            ).date()
+        self.conf["execution_date"] = execution_date
+
     def _read_delta_table(self, input_dict: dict[str, str]) -> DataFrame:
         df = read_delta_table(
             self.spark, input_dict["database"], input_dict["table"]
@@ -90,62 +47,17 @@ class EvaluationTask(Task):
             output_dict["table"],
         )
 
-    def get_metrics(
-        self, df_test: DataFrame, df_forecast_on_test: DataFrame
-    ) -> DataFrame:
-        df = df_test.withColumnRenamed(self.conf["target_column"], "y").join(
-            df_forecast_on_test.withColumnRenamed(
-                self.conf["target_column"], "y_pred"
-            ),
-            on=["store", "item", "date"],
-            how="left",
-        )
-
-        metrics = [
-            getattr(darts.metrics, metric) for metric in self.conf["metrics"]
-        ]
-        group_columns: list[str] = self.conf["group_columns"]
-        time_column: str = self.conf["time_column"]
-        target_column: str = self.conf["target_column"]
-        df_metrics = df.groupBy(["model", "store", "item"]).applyInPandas(
-            lambda pdf: Metrics.get_metrics(
-                df=pdf,
-                metrics=metrics,
-                group_columns=group_columns,
-                time_column=time_column,
-                target_column=target_column,
-            ),
-            schema=MetricsSchema,
-        )
-        return df_metrics
-
-    def get_best_models(self, df_metrics: DataFrame) -> DataFrame:
-        df_best_models = (
-            df_metrics.filter(
-                F.col("metric") == self.conf["model_selection_metric"]
-            )
-            .groupBy(self.conf["group_columns"])
-            .agg(F.min(F.struct("value", "model")).alias("arr"))
-            .withColumn("model", F.col("arr.model"))
-            .withColumn("value", F.col("arr.value"))
-            .drop("arr")
-            .withColumn("metric", F.lit(self.conf["model_selection_metric"]))
-            .select(*MetricsSchema.fieldNames())
-        )
-        return df_best_models
-
-    def get_forecast(
-        self, df_all_models_forecast: DataFrame, df_best_models: DataFrame
-    ) -> DataFrame:
-        df_forecast = df_all_models_forecast.join(
-            df_best_models.drop("metric", "value"),
-            on=["model"] + self.conf["group_columns"],
-            how="inner",
-        )
-        return df_forecast
-
     def launch(self):
         self.logger.info(f"Launching {self.__class__.__name__}")
+
+        evaluation = Evaluation(
+            group_columns=self.conf["group_columns"],
+            time_column=self.conf["time_column"],
+            target_column=self.conf["target_column"],
+            metrics=self.conf["metrics"],
+            model_selection_metric=self.conf["model_selection_metric"],
+            freq=self.conf["freq"],
+        )
 
         mlflow.set_experiment(self.conf["experiment"])
         with mlflow.start_run(run_name=self.__class__.__name__):
@@ -157,8 +69,10 @@ class EvaluationTask(Task):
             df_forecast_on_test = self._read_delta_table(
                 self.conf["input"]["forecast_on_test"]
             )
-            df_metrics = self.get_metrics(df_test, df_forecast_on_test)
-            df_best_models = self.get_best_models(df_metrics).cache()
+            df_metrics = evaluation.get_metrics(
+                df_test, df_forecast_on_test, MetricsSchema
+            )
+            df_best_models = evaluation.get_best_models(df_metrics).cache()
 
             mlflow.log_metric(
                 self.conf["model_selection_metric"],
@@ -168,7 +82,9 @@ class EvaluationTask(Task):
         df_all_models_forecast = self._read_delta_table(
             self.conf["input"]["all_models_forecast"]
         )
-        df_forecast = self.get_forecast(df_all_models_forecast, df_best_models)
+        df_forecast = evaluation.get_forecast(
+            df_all_models_forecast, df_best_models
+        )
 
         self._write_delta_table(
             df_metrics, MetricsSchema, self.conf["output"]["metrics"]
