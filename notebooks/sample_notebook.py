68bbd6d1505e3b92cb1ffd773c25c2e5d490ee7b
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2023-11-24 22:09:39 -0300

    feat: update sample_notebook with SplitTask

--- notebooks/sample_notebook.py
+++ notebooks/sample_notebook.py
@@ -22,7 +22,28 @@
 
 # COMMAND ----------
 
-from dbx_demand_forecast.tasks.sample_ml_task import SampleMLTask
+from pathlib import Path
 
-pipeline = SampleMLTask.get_pipeline()
-print(pipeline)
+import yaml
+from pyspark.sql import SparkSession
+
+from spark_forecast.tasks.split import SplitTask
+
+# COMMAND ----------
+
+project_root = Path(".").absolute().parent
+
+# COMMAND ----------
+
+spark = SparkSession.builder.getOrCreate()
+spark.sql("USE CATALOG `demand-forecast`")
+
+# COMMAND ----------
+
+conf_file = f"{project_root}/conf/tasks/split_config.yml"
+conf = yaml.safe_load(Path(project_root, conf_file).read_text())["env"]["dev"]
+
+# COMMAND ----------
+
+task = SplitTask(spark, conf)
+task.launch()
