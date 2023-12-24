4ddd72ed23e1a6f862de18af8cffacd2256d4634
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2023-12-24 18:41:02 -0300

    refactor(test): add symmetric behavior to assert_pyspark_df_equal and ignore_order arg

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,49 @@
+from typing import Optional
+
+import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
+from pyspark.sql.types import NumericType
 
 
 def assert_pyspark_df_equal(
-    expected_df: DataFrame, actual_df: DataFrame
+    expected_df: DataFrame,
+    actual_df: DataFrame,
+    round: Optional[int] = None,
+    ignore_order: bool = True,
 ) -> None:
-    equal_schema = expected_df.dtypes == actual_df.dtypes
-    equal_count = expected_df.count() == actual_df.count()
-    equal_values = expected_df.join(actual_df, how="anti").count() == 0
-    assert equal_schema and equal_count and equal_values
+    assert (
+        expected_df.dtypes == actual_df.dtypes
+    ), "DataFrame schemas are different"
+
+    if round is not None:
+        for col in (
+            field.name
+            for field in expected_df.schema
+            if isinstance(field.dataType, NumericType)
+        ):
+            expected_df = expected_df.withColumn(col, F.round(col, 2))
+            actual_df = actual_df.withColumn(col, F.round(col, 2))
+
+    if not ignore_order:
+        expected_df = expected_df.withColumn(
+            "_index", F.monotonically_increasing_id()
+        )
+        actual_df = actual_df.withColumn(
+            "_index", F.monotonically_increasing_id()
+        )
+
+    assert (
+        expected_df.count() == actual_df.count()
+    ), "DataFrame row counts are different"
+
+    def are_dfs_equal(df1, df2):
+        if ignore_order:
+            df1 = df1.orderBy([F.col(c).asc_nulls_last() for c in df1.columns])
+            df2 = df2.orderBy([F.col(c).asc_nulls_last() for c in df2.columns])
+        return (
+            df1.subtract(df2).count() == 0 and df2.subtract(df1).count() == 0
+        )
+
+    assert are_dfs_equal(
+        expected_df, actual_df
+    ), "DataFrames contain different data"
